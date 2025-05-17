d0d7190f00ecc68b7b6f52c78a58764fc199dc8a
TG. MANIKANTA <tgmanikanta11@gmail.com>
2025-05-17 17:54:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > ./tailwind.config.ts:100:4
    > Parsing ecmascript source code failed
    >    98 |   plugins: [require("tailwindcss-animate")],
    >    99 | } satisfies Config;
    > > 100 | ```
    >       |    ^
    >
    > Unexpected eof
    >
    >

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-const config = {
+const config: Config = {
   darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -89,6 +89,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
+};
 
 export default config;
